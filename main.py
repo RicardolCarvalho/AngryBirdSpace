5e6fcf9ead88191a5525d74d1e1a28014be0ae8c
RicardolCarvalho <ricardolc2@al.insper.edu.br>
2024-08-13 12:20:38 -0300

    corpo

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 import pygame
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.screen.fill((0, 0, 0))
+            pygame.display.flip()
+            self.clock.tick(60)
+
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
